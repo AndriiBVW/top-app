f0daa5d24c9975c9d7fed08f788d8319c9954192
AndriiBVW <andriibvw@gmail.com>
2021-11-11 21:30:51 +0200

    feat: add document template

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css';
+import Head from "next/head";
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<title>MyTop - наш лучший топ</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
