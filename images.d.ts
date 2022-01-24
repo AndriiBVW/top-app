02f4d4332ce2b5caf8c79d8c4b699a6e6246577e
AndriiBVW <andriibvw@gmail.com>
2022-01-24 16:25:29 +0200

    feat: finished work

--- images.d.ts
+++ images.d.ts
@@ -11,7 +11,7 @@ interface StaticImageData {
 	src: string
 	height: number
 	width: number
-	blurDataURL?: string
+	placeholder?: string
 }
 
 declare module '*.png' {
@@ -19,44 +19,33 @@ declare module '*.png' {
 	export default content;
 }
 
+
 declare module '*.jpg' {
 	const content: StaticImageData;
-
 	export default content;
 }
 
 declare module '*.jpeg' {
 	const content: StaticImageData;
-
 	export default content;
 }
 
 declare module '*.gif' {
 	const content: StaticImageData;
-
 	export default content;
 }
 
 declare module '*.webp' {
 	const content: StaticImageData;
-
-	export default content;
-}
-
-declare module '*.avif' {
-	const content: StaticImageData;
-
 	export default content;
 }
 
 declare module '*.ico' {
 	const content: StaticImageData;
-
 	export default content;
 }
 
 declare module '*.bmp' {
 	const content: StaticImageData;
-
 	export default content;
 }
--- next.config.js
+++ next.config.js
@@ -14,7 +14,7 @@ module.exports = {
 						name: 'preset-default',
 						params: {
 							override: {
-								removeVyewBox: false
+								removeViewBox: false
 							}
 						}
 					}],
